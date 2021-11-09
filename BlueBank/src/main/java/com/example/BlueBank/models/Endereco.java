9950c2518a454b00e34cd69dad89c03782b05b88
zecarlos558 <45573159+zecarlos558@users.noreply.github.com>
2021-11-08 21:33:54 -0300

    Subindo Classe Model Endereço

--- BlueBank/src/main/java/com/example/BlueBank/models/Endereco.java
+++ BlueBank/src/main/java/com/example/BlueBank/models/Endereco.java
@@ -1,7 +1,129 @@
 package com.example.BlueBank.models;
 
-public class Endereco {
+import java.io.Serializable;
+import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@NotEmpty
+	@OneToOne(mappedBy = "endereco")
+	private Cliente idCliente;
+	@NotEmpty
+	private String logradouro;
+	@NotEmpty
+	private String cidade;
+	@NotEmpty
+	private String estado;
+	@NotEmpty
+	private int numero;
+	@NotEmpty
+	private String cep;
+	
+	public Endereco() {
+		super();
+	}
+
+	public Endereco(@NotEmpty Cliente idCliente, @NotEmpty String logradouro, @NotEmpty String cidade,
+			@NotEmpty String estado, @NotEmpty int numero, @NotEmpty String cep) {
+		super();
+		this.idCliente = idCliente;
+		this.logradouro = logradouro;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.numero = numero;
+		this.cep = cep;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Cliente getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Cliente idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cep, id, idCliente);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(cep, other.cep) && Objects.equals(id, other.id)
+				&& Objects.equals(idCliente, other.idCliente);
+	}
+	
 	
 	
 }
