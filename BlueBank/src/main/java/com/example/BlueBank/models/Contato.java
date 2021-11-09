f784ae7ddc5ca4ff061c1f0bdb3df3c7a7cd09d6
carmoPjunior <carmo.goncalves.jr@hotmail.com>
2021-11-08 21:32:27 -0300

    Adiciona a classe contato

--- BlueBank/src/main/java/com/example/BlueBank/models/Contato.java
+++ BlueBank/src/main/java/com/example/BlueBank/models/Contato.java
@@ -1,5 +1,121 @@
 package com.example.BlueBank.models;
 
-public class Contato {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Contato implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "idCliente", nullable = false)
+	private Cliente cliente;
+
+	@NotEmpty
+	private String numeroTelefone;
+
+	@NotEmpty
+	@Email
+	private String email;
+
+	public Contato() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Contato(Cliente cliente, @NotEmpty String numeroTelefone, @NotEmpty @Email String email) {
+		super();
+		this.cliente = cliente;
+		this.numeroTelefone = numeroTelefone;
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public String getNumeroTelefone() {
+		return numeroTelefone;
+	}
+
+	public void setNumeroTelefone(String numeroTelefone) {
+		this.numeroTelefone = numeroTelefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((numeroTelefone == null) ? 0 : numeroTelefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (cliente == null) {
+			if (other.cliente != null)
+				return false;
+		} else if (!cliente.equals(other.cliente))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (numeroTelefone == null) {
+			if (other.numeroTelefone != null)
+				return false;
+		} else if (!numeroTelefone.equals(other.numeroTelefone))
+			return false;
+		return true;
+	}
 
 }
