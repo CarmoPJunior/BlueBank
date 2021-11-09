afc090afeebf16bb1157bd0888cb17fd6bf44312
CarmoPJunior <carmo.goncalves.jr@hotmail.com>
2021-11-08 21:46:04 -0300

    Altera a classe Tipo Conta
    Altera a classe Tipo Conta

--- BlueBank/src/main/java/com/example/BlueBank/models/TipoConta.java
+++ BlueBank/src/main/java/com/example/BlueBank/models/TipoConta.java
@@ -1,7 +1,15 @@
 package com.example.BlueBank.models;
 
 
-public class TipoConta {
+public enum TipoConta {
 	
+	CORRENTE(1),POUPANCA(2), SALARIO(3);
+
+	public int id;
+	
+	TipoConta(int id) {
+		
+		this.id = id;
+	}
 
 }
